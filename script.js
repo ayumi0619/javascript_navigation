1f7cfd311e04caecf5b0a7b2c8a35a4e6fda75f8
ayumi0619 <108720099+ayumi0619@users.noreply.github.com>
2024-01-06 01:16:51 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,51 +1,66 @@
 // JavaScript
-//console.log('Hello world!');
 const open = document.querySelector('#btn-open');
 const close = document.querySelector('#btn-close');
 const menu = document.querySelector('#menu');
-
 const items = document.querySelectorAll('.item');
-  const menuOpen = () => {
-    const keyframes = {
-      visibility: ['hidden','visible'],
-      opacity: [0,1]
-    }
-    const options = {
+
+const options = {
+  duration: 600,
+  easing: 'ease',
+  fill: 'forwards',
+}
+
+
+const menuOpen = () => {
+  const keyframes = {
+    visibility: ['hidden','visible'],
+    opacity: [0,1]
+  }
+  menu.animate(keyframes,options);
+
+  items.forEach((item,index) => {
+    item.animate({opacity:[0,1]},{
       duration: 600,
       easing: 'ease',
       fill: 'forwards',
-    }
-    menu.animate(keyframes,options);
-    items.forEach((item,index) => {
-        item.animate({opacity:[0,1]},{
-          duration: 600,
-          easing: 'ease',
-          fill: 'forwards',
-          delay: index * 200,
-        });
-      });
+      delay: index * 200,
+    });
+  });
+
+}
+open.addEventListener('click',menuOpen);
+
+const menuClose = () => {
+  const keyframes = {
+    visibility: ['visible','hidden'],
+    opacity: [1,0]
   }
-  open.addEventListener('click',menuOpen);
+  menu.animate(keyframes,options);
 
-
-  const menuClose = () => {
-    const keyframes = {
-      visibility: ['visible','hidden'],
-      opacity: [1,0]
-    }
-    const options = {
+  items.forEach((item) => {
+    item.animate({opacity:[1,0]},{
       duration: 600,
       easing: 'ease',
       fill: 'forwards',
-    }
-    menu.animate(keyframes,options);
+    });
+  });
 
-    items.forEach((item) => {
-        item.animate({opacity:[1,0]},{
-          duration: 600,
-          easing: 'ease',
-          fill: 'forwards',
-        });
-      });
-  }
-  close.addEventListener('click',menuClose);
+}
+close.addEventListener('click',menuClose);
+console.log('Hello world!');
+
+// 色が変わる
+const color = document.querySelector('.color');
+
+const colorKeyframes = {
+  backgroundColor: ['orange ','yellow ','lightgreen','skyblue'],
+}
+
+const colorOptions = {
+  duration: 5000,
+  direction: 'alternate',
+  iterations: Infinity,
+}
+
+color.animate(colorKeyframes,colorOptions);
+
